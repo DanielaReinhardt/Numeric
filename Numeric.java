1894a8f375321ff5c4c0463c759859904bc37607
DanielaReinhardt <reinhardtd@telekom.de>
2022-03-03 12:57:14 +0100

    new version

--- Numeric.java
+++ Numeric.java
@@ -1,21 +1,40 @@
 
 public class Numeric{
-    static void myMethod(int x){
+    static void myMethod1(int x){
        System.out.print("The integer value is ");
             if(x%2==0){
                 System.out.println("even.");
             } else{
                 System.out.println("odd.");
             }
-            System.out.print("The square of the integer is ");
-            System.out.println(x*x);
-            System.out.print("The cube of the integer is ");
-            System.out.println(x*x*x);
-           
+               
         }
+
+    static void myMethod2(int a){
+        System.out.print("The integer value is ");
+            if(a%2==0){
+                     System.out.println("even.");
+             } else{
+                     System.out.println("odd.");
+                 }
+                    
+             }
+
+
+    static void myMethod3(int z){
+        System.out.print("The square of the integer is ");
+        System.out.println(z*z);
+    }
+
+    static void myMethod4(int y){
+        System.out.print("The cube of the integer is ");
+        System.out.println(y*y*y);
+
+    }
+    
     public static void main(String[] args) {
-    myMethod(6);
-    myMethod(5);
+    myMethod3(6);
+    myMethod1(5);
    
 }
 }
